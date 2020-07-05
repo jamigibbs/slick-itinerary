6751480717338875d81db105fdbfc84f95235d47
Jami Gibbs <jami0821@gmail.com>
2020-07-05 14:40:22 -0500

    Display cover image and header style update.

--- src/ItineraryCard.js
+++ src/ItineraryCard.js
@@ -14,7 +14,9 @@ class ItineraryCard extends React.Component {
 
   render() {
     const { card } = this.props;
-    const{ desc, customFieldItems } = card;
+    const{ desc, customFieldItems, cover } = card;
+    
+    const coverUrl = cover.url ? cover.url : cover.sharedSourceUrl;
 
     const address = customFieldItems.find(field => {
       return field.name.toLowerCase() === 'address';
@@ -28,9 +30,11 @@ class ItineraryCard extends React.Component {
       return field.name.toLowerCase() === 'start time';
     });
 
-    const startTime = this.formatedTime(startTimeField.value.date);
+    let startTime = null;
+    if (startTimeField) {
+      startTime = this.formatedTime(startTimeField.value.date);
+    }
   
-    
     // The color "sky" does not map to the ant design Tag component so
     // we're just assigning the color as blue.
     const labels = card.labels.map((label) => {
@@ -40,31 +44,44 @@ class ItineraryCard extends React.Component {
       return label;
     });
 
-    const googleMapLink = `https://maps.google.com/?q=1200 ${address.value.text}`;
+    let googleMapLink = null;
+    if (address && address.value.text) {
+      googleMapLink = `https://maps.google.com/?q=1200 ${address.value.text}`;
+    }
 
     return (
       <div className="itinerary-card">
         {startTime && <div className="card-start-time">{startTime}</div>}
-        {link 
-          ? <h4 className="card-name"><a href={link.value.text}>{card.name}</a></h4>
-          :  <h4 className="card-name">{card.name}</h4>
-        }
-        {address && 
-          <div className="card-address">
-            <Link href={googleMapLink} target="_blank">{address.value.text}</Link>
+        <div>
+          <div className="header-left-column">
+            {link 
+              ? <h4 className="card-name"><a href={link.value.text}>{card.name}</a></h4>
+              :  <h4 className="card-name">{card.name}</h4>
+            }
+            {address && 
+              <div className="card-address">
+                <Link href={googleMapLink} target="_blank">{address.value.text}</Link>
+              </div>
+            }
           </div>
-        }
+          <div className="header-right-column">
+            {coverUrl && 
+              <img className="card-cover" src={coverUrl} alt={card.name}></img>
+            }
+          </div>
+
+        </div>
         {desc &&
           <div className="card-desc">
             <ReactMarkdown class="card-desc" source={desc} />
           </div>
         }
         <div className="card-labels">
-        {labels.map(label => {
-          return (
-            <Tag key={label.id} color={label.color}>{label.name}</Tag>
-          )
-        })}
+          {labels.map(label => {
+            return (
+              <Tag key={label.id} color={label.color}>{label.name}</Tag>
+            )
+          })}
         </div>
 
       </div>
