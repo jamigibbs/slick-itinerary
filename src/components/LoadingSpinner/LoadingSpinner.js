8808b4dde6c3628645a421f5b6575eeb9724806f
Jami Gibbs <jami0821@gmail.com>
2020-07-27 16:51:22 -0500

    Refactor LoadingSpinner component from class to function.

--- src/components/LoadingSpinner/LoadingSpinner.js
+++ src/components/LoadingSpinner/LoadingSpinner.js
@@ -1,27 +1,24 @@
-import React from "react";
-import { css } from "@emotion/core";
-import PuffLoader from "react-spinners/PuffLoader";
+import React from 'react';
+import { css } from '@emotion/core';
+import PuffLoader from 'react-spinners/PuffLoader';
 
 const override = css`
   display: block;
   margin: 0 auto;
 `;
 
-class LoadingSpinner extends React.Component {
-
-  render() {
-    const { isLoading } = this.props;
-    return (
-      <div className="sweet-loading">
-        <PuffLoader
-          css={override}
-          size={100}
-          color={"#47D7B7"}
-          loading={isLoading}
-        />
-      </div>
-    );
-  }
+const LoadingSpinner = (props) => {
+  const { isLoading } = props;
+  return (
+    <div className="sweet-loading">
+      <PuffLoader
+        css={override}
+        size={100}
+        color={"#47D7B7"}
+        loading={isLoading}
+      />
+    </div>
+  );
 }
 
 export default LoadingSpinner;
