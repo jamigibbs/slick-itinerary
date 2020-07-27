adac07eabfc91a563b0108c33c12792f46cacaee
Jami Gibbs <jami0821@gmail.com>
2020-07-26 21:27:26 -0500

    Moving client side fetch to express server.

---
+++ server.js
@@ -0,0 +1,158 @@
+const axios = require('axios').default;
+const path = require("path");
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+
+const port = process.env.PORT || 5000;
+
+require("dotenv").config();
+
+const TRELLO_API_ROOT = 'https://api.trello.com/1';
+const TRELLO_KEY = process.env.REACT_APP_TRELLO_KEY;
+const TRELLO_TOKEN = process.env.REACT_APP_TRELLO_TOKEN;
+
+app.use(morgan('tiny'));
+
+app.get('/api/board/:boardShortLink', async function (req, res) {
+  const boardShortLink = req.params.boardShortLink;
+  try {
+    const itinerary = await getTrelloBoards(boardShortLink);
+    let customFields = [];
+
+    if (itinerary.id) {
+      const boardListsData = await getTrelloBoardLists(boardShortLink);
+
+      itinerary.lists = boardListsData;
+      itinerary.lists.forEach((item) => {
+        // Adding a place to push cards assigned to the list for the next fetch.
+        item.cards = [];
+      });
+      const boardCustomFields = await getBoardCustomFields(boardShortLink);
+      customFields = boardCustomFields;
+
+      const boardCards = await getBoardCards(boardShortLink);
+      // Getting the cards that have a cover ID for us to fetch the image.
+      const cardsWithCovers = boardCards.cards.filter((item, i) => {
+        return item.cover.idAttachment;
+      });
+
+      // Creating our batched request array for fetching cover urls. 
+      const batchedUrls = [];
+      let batch = [];
+
+      for (let i = 0; i < cardsWithCovers.length; i++) {
+        // if it's the 10th item, add array to batchedUrls and reset the batch array.
+        const url = `/cards/${cardsWithCovers[i].id}/attachments/${cardsWithCovers[i].cover.idAttachment}`;
+        batch.push(url);
+        // If we have our max 10 request urls, or it's the last item, push the request url group and reset batch array.
+        if (batch.length === 10 || i === cardsWithCovers.length - 1) {
+          batchedUrls.push(batch);
+          batch = [];
+        }
+
+        let requests = batchedUrls.map(async (urls) => {
+          // return fetch(`${TRELLO_API_ROOT}/batch?key=${TRELLO_KEY}&token=${TRELLO_TOKEN}&urls=${urls}`)
+          return await getTrelloBatch(urls);
+        });
+
+        Promise.all(requests)
+          .then(responses => Promise.all(responses.map(response => response.json())))
+          .then(batchedData => {
+            // Combine the batched array responses into a single array.
+            const mergedCardsWithCovers = [].concat.apply([], batchedData);
+
+            boardCards.cards.forEach((card) => {
+              // Finding which list the card goes and putting it into that list.
+              const list = itinerary.lists.find((item) => {
+                return item.id === card.idList
+              });
+
+              // Finding the card cover image url and adding it to the card.
+              const cover = mergedCardsWithCovers.find((cardWithCover) => {
+                return card.cover.idAttachment === cardWithCover[200].id;
+              });
+              if (cover) card.cover.url = cover[200].url;
+        
+              // Mapping the custom field name to the custom field value.
+              card.customFieldItems.map((fieldItem) => {
+                const matchingCustomField = customFields.find((field) => {
+                  return field.id === fieldItem.idCustomField
+                });
+                if (matchingCustomField) fieldItem.name = matchingCustomField.name;
+                return fieldItem;
+              });
+        
+              list.cards.push(card);
+            });
+
+            return res.status(200).json(itinerary);
+          });
+          return;
+      }
+    }
+  } catch(error) {
+    return res.status(400).send(error);
+  }
+});
+
+async function getTrelloBoards(boardShortLink){
+  try {
+    const { data } = await axios.get(`${TRELLO_API_ROOT}/boards/${boardShortLink}?key=${TRELLO_KEY}&token=${TRELLO_TOKEN}&fields=name,url,prefs,cards,shortLink`);
+    return data;
+  } catch(error) {
+    console.error('getTrelloBoards error', error);
+    return;
+  }
+}
+
+async function getTrelloBoardLists(boardShortLink){
+  try {
+    const { data } = await axios.get(`${TRELLO_API_ROOT}/boards/${boardShortLink}/lists?key=${TRELLO_KEY}&token=${TRELLO_TOKEN}`);
+    return data;
+  } catch(error) {
+    console.error('getTrelloBoardLists error', error);
+    return;
+  }
+}
+
+async function getBoardCustomFields(boardShortLink){
+  try {
+    const { data } = await axios.get(`${TRELLO_API_ROOT}/boards/${boardShortLink}/customFields?key=${TRELLO_KEY}&token=${TRELLO_TOKEN}`);
+    return data;
+  } catch(error) {
+    console.error('getBoardCustomFields error', error);
+    return;
+  }
+}
+
+async function getTrelloBatch(urls){
+  try {
+    const { data } = await axios.get(`${TRELLO_API_ROOT}/batch?key=${TRELLO_KEY}&token=${TRELLO_TOKEN}&urls=${urls}`);
+    return data;
+  } catch(error) {
+    console.error('getTrelloBatch error', error);
+    return;
+  }
+}
+
+async function getBoardCards(boardShortLink){
+  try {
+    const { data } = await axios.get(`${TRELLO_API_ROOT}/boards/${boardShortLink}?cards=visible&card_customFieldItems=true&key=${TRELLO_KEY}&token=${TRELLO_TOKEN}&attachments=true&attachment_fields=all`);
+    return data;
+  } catch(error) {
+    return;
+  }
+}
+
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static(path.join(__dirname, "build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+  });
+}
+
+app.listen(port, () => {
+  console.log(`Server is on up ${port} now !`);
+});
--- src/components/Itinerary/Itinerary.js
+++ src/components/Itinerary/Itinerary.js
@@ -9,11 +9,11 @@ import './Itinerary.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSuitcase} from '@fortawesome/free-solid-svg-icons'
 import { MehOutlined } from '@ant-design/icons';
-import { TRELLO_API_ROOT, TRELLO_KEY, TRELLO_TOKEN, ACCENT_COLOR_KEY } from '../../constants';
+import { ACCENT_COLOR_KEY } from '../../constants';
 
 class Itinerary extends React.Component {
   state = {
-    isLoading: true,
+    isLoading: false,
     itinerary: {},
     error: null,
     accentColor: '#E1474A',
@@ -22,7 +22,16 @@ class Itinerary extends React.Component {
   componentDidMount() {
     const { props } = this.props;
     const boardShortLink = props.match.params.boardShortLink;
-    this.fetchItinerary(boardShortLink);
+    this.setState({isLoading: true});
+
+    fetch(`/api/board/${boardShortLink}`)
+      .then((response) => response.json())
+      .then((itinerary) => {
+        if (isLocalHost()) console.log('itinerary', itinerary);
+        const itineraryColorKey = ACCENT_COLOR_KEY + '_' + itinerary.shortLink;
+        const accentColor = localStorage.getItem(itineraryColorKey);
+        this.setState({itinerary, accentColor, isLoading: false});
+      })
   }
 
   handleLoadingIndicator(bool){
@@ -43,125 +52,6 @@ class Itinerary extends React.Component {
     // Setting the selected color into local storage.
     const itineraryColorKey = ACCENT_COLOR_KEY + '_' + this.state.itinerary.shortLink;
     localStorage.setItem(itineraryColorKey, accentColor);
-  }
-
-  fetchItinerary(boardShortLink) {
-    let boardId = null;
-    let itinerary = null;
-    let customFields = [];
-
-    fetch(`${TRELLO_API_ROOT}/members/me/boards?fields=name,url,prefs,cards,shortLink&key=${TRELLO_KEY}&token=${TRELLO_TOKEN}`)
-      .then(response => response.json())
-      .then((data) => {
-
-        itinerary = data.find((item) => {
-          return item.shortLink === boardShortLink;
-        })
-
-        if (itinerary.id) {
-          boardId = itinerary.id;
-
-          // Checking if there's a selected accent color available yet.
-          const itineraryColorKey = ACCENT_COLOR_KEY + '_' + itinerary.shortLink;
-          const accentColor = localStorage.getItem(itineraryColorKey);
-          if (accentColor) this.setState({accentColor});
-
-          fetch(`${TRELLO_API_ROOT}/boards/${boardId}/lists?key=${TRELLO_KEY}&token=${TRELLO_TOKEN}`)
-            .then((response) => response.json())
-            .then((data) => {
-              // board card info
-              itinerary.lists = data;
-              itinerary.lists.forEach((item) => {
-                // Adding a place to push cards assigned to the list for the next fetch.
-                item.cards = [];
-              });
-          
-              return fetch(`${TRELLO_API_ROOT}/boards/${boardId}/customFields?key=${TRELLO_KEY}&token=${TRELLO_TOKEN}`)
-            })
-            .then((response) => response.json())
-            .then((data) => {
-              // Getting custom field data so that we can map the field name.
-              customFields = data;
-              // Get the cards and map them to their assigned list.
-              return fetch(`${TRELLO_API_ROOT}/boards/${boardId}?cards=visible&card_customFieldItems=true&key=${TRELLO_KEY}&token=${TRELLO_TOKEN}&attachments=true&attachment_fields=all`);
-            })
-            .then((response) => response.json())
-            .then((data) => {
-              // Getting the cards that have a cover ID for us to fetch the image.
-              const cardsWithCovers = data.cards.filter((item, i) => {
-                return item.cover.idAttachment;
-              });
-
-              // Creating our batched request array for fetching cover urls. 
-              const batchedUrls = [];
-              let batch = [];
-
-              for (let i = 0; i < cardsWithCovers.length; i++) {
-                // if it's the 10th item, add array to batchedUrls and reset the batch array.
-                const url = `/cards/${cardsWithCovers[i].id}/attachments/${cardsWithCovers[i].cover.idAttachment}`;
-                batch.push(url);
-                // If we have our max 10 request urls, or it's the last item, push the request url group and reset batch array.
-                if (batch.length === 10 || i === cardsWithCovers.length - 1) {
-                  batchedUrls.push(batch);
-                  batch = [];
-                }
-              }
-
-              let requests = batchedUrls.map((urls) => {
-                return fetch(`${TRELLO_API_ROOT}/batch?key=${TRELLO_KEY}&token=${TRELLO_TOKEN}&urls=${urls}`)
-              });
-
-              Promise.all(requests)
-                .then(responses => Promise.all(responses.map(res => res.json())))
-                .then(batchedData => {
-
-                  // Combine the batched array responses into a single array.
-                  const mergedCardsWithCovers = [].concat.apply([], batchedData);
-
-                  data.cards.forEach((card) => {
-                    // Finding which list the card goes and putting it into that list.
-                    const list = itinerary.lists.find((item) => {
-                      return item.id === card.idList
-                    });
-
-                    // Finding the card cover image url and adding it to the card.
-                    const cover = mergedCardsWithCovers.find((cardWithCover) => {
-                      return card.cover.idAttachment === cardWithCover[200].id;
-                    });
-                    if (cover) card.cover.url = cover[200].url;
-              
-                    // Mapping the custom field name to the custom field value.
-                    card.customFieldItems.map((fieldItem) => {
-                      const matchingCustomField = customFields.find((field) => {
-                        return field.id === fieldItem.idCustomField
-                      });
-                      if (matchingCustomField) fieldItem.name = matchingCustomField.name;
-                      return fieldItem;
-                    });
-              
-                    list.cards.push(card);
-                  });
-
-                  // Set the page title to the itinerary name
-                  document.title = `${itinerary.name} | Slick Itinerary`;
-
-                  if (isLocalHost()) {
-                    console.log('itinerary', itinerary) 
-                  }
-
-                  this.setState({
-                    itinerary,
-                    isLoading: false,
-                  });
-                });
-          
-            })
-            .catch(error => this.setState({ error, isLoading: false }));
-        } else {
-          this.setState({ error: 'Sorry, no board found.', isLoading: false })
-        }
-      })
-      .catch((error) => this.setState({ error: 'Sorry, no board found.', isLoading: false }));
   }
 
   render() {
