6ad9851fd531ada125d2f411773bc5f2398eed40
Jami Gibbs <jami0821@gmail.com>
2020-07-27 16:44:43 -0500

    Refactor Home component from class to function.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
 import LinkInput from '../LinkInput';
 import BoardHistory from '../BoardHistory';
@@ -10,66 +10,63 @@ import { BOARD_HISTORY_KEY, TRELLO_URL } from '../../constants';
 const { Footer, Content } = Layout;
 const { Title, Paragraph, Text } = Typography;
 
-class Home extends React.Component {
-  state = {
-    boardHistory: []
-  }
+const Home = (props) => {
+  const [boardHistory, setBoardHistory] = useState([]);
 
-  componentDidMount() {
+  useEffect(() => {
+    getBoardHistory();
+  });
+
+  const getBoardHistory = () => {
     const boardHistory = JSON.parse(localStorage.getItem(BOARD_HISTORY_KEY));
-    this.setState({boardHistory});
+    setBoardHistory(boardHistory);
   }
 
-  render() {
-    const { boardHistory } = this.state;
-    let isBoardHistory = false;
-    if (boardHistory) {
-      isBoardHistory = boardHistory.length > 0;
-    }
-    return (
+  const isBoardHistory = boardHistory ? boardHistory.length > 0 : false;
+
+  return (
+    <Layout>
+      <div className="flex-wrapper">
       <Layout>
-        <div className="flex-wrapper">
-        <Layout>
-          <Content style={{position: 'relative'}}>
-            <Row className="content-row">
-              <Col xs={{ span: 24, order: 2 }} sm={{ span: 24, order: 2 }} md={{ span: 12, order: 1 }} className="home-left-column-wrap">
-                <div className="home-left-column">
-                  <Title className="logo">Slick Itinerary</Title>
-                  <Paragraph>
-                  Organize your trip using <a href={TRELLO_URL} target="_blank" rel="noopener noreferrer">Trello</a> and turn it into a sharable, printable, beautiful travel itinerary instantly!
-                  </Paragraph>
+        <Content style={{position: 'relative'}}>
+          <Row className="content-row">
+            <Col xs={{ span: 24, order: 2 }} sm={{ span: 24, order: 2 }} md={{ span: 12, order: 1 }} className="home-left-column-wrap">
+              <div className="home-left-column">
+                <Title className="logo">Slick Itinerary</Title>
+                <Paragraph>
+                Organize your trip using <a href={TRELLO_URL} target="_blank" rel="noopener noreferrer">Trello</a> and turn it into a sharable, printable, beautiful travel itinerary instantly!
+                </Paragraph>
 
-                  <Divider />
+                <Divider />
 
-                  <Text strong>Quick Start</Text>
+                <Text strong>Quick Start</Text>
 
-                  <Paragraph>
-                  Copy our <a href={`${TRELLO_URL}/b/I0TyGYpi`} target="_blank" rel="noopener noreferrer">example trip board</a> and modify it for your own trip. Then enter your board link below to generate an itineray.
-                  </Paragraph>
+                <Paragraph>
+                Copy our <a href={`${TRELLO_URL}/b/I0TyGYpi`} target="_blank" rel="noopener noreferrer">example trip board</a> and modify it for your own trip. Then enter your board link below to generate an itineray.
+                </Paragraph>
 
-                  <Paragraph type="secondary" style={{marginBottom: '20px'}}>Need more help? Check out the <Link to="guide">support guide</Link>.</Paragraph>
+                <Paragraph type="secondary" style={{marginBottom: '20px'}}>Need more help? Check out the <Link to="guide">support guide</Link>.</Paragraph>
 
-                  <LinkInput />
+                <LinkInput />
 
-                  <Divider dashed={true} />
+                <Divider dashed={true} />
 
-                  { isBoardHistory && <BoardHistory data={boardHistory} /> }
-                </div>
-              </Col>
-              <Col className="home-right-column" xs={{ span: 24, order: 1 }} sm={{ span: 24, order: 1 }} md={{ span: 12, order: 2 }} >
-                <img src="home-bg.svg" alt="woman riding bike" />
-              </Col>
-            </Row>
-          </Content>
-        </Layout>
-        <Footer style={{backgroundColor: '#333333'}}>
-          <SiteFooter></SiteFooter>
-        </Footer>
-        </div>
+                { isBoardHistory && <BoardHistory data={boardHistory} /> }
+              </div>
+            </Col>
+            <Col className="home-right-column" xs={{ span: 24, order: 1 }} sm={{ span: 24, order: 1 }} md={{ span: 12, order: 2 }} >
+              <img src="home-bg.svg" alt="woman riding bike" />
+            </Col>
+          </Row>
+        </Content>
       </Layout>
+      <Footer style={{backgroundColor: '#333333'}}>
+        <SiteFooter></SiteFooter>
+      </Footer>
+      </div>
+    </Layout>
 
-    )
-  }
+  )
 }
 
 export default Home;
