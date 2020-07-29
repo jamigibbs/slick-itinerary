5becbd7d1fb3f8aa90cc29b69d6def971b3143e1
Jami Gibbs <jami0821@gmail.com>
2020-07-29 10:40:26 -0500

    Convert ItineraryCards from class to function.

--- src/components/ItineraryCards/ItineraryCards.js
+++ src/components/ItineraryCards/ItineraryCards.js
@@ -3,28 +3,24 @@ import './ItineraryCards.css';
 import ItineraryCard from'../ItineraryCard';
 import { Divider } from 'antd';
 
-class ItineraryCards extends React.Component {
-
-  render() {
-    const { cards, accentColor } = this.props;
-    const cardsCount = cards.length;
-    return (
-      <div>
-        <ul className="cards-list">
-          {cards.map((card, i) => {
-            return (
-              <li key={card.id}>
-                <span className="cards-start-circle"></span>
-                <ItineraryCard card={card} accentColor={accentColor} />
-                {cardsCount !== i + 1 && <Divider />}
-              </li>
-            )
-          })}
-        </ul>
-      </div>
-    )
-  }
-
+const ItineraryCards = (props) => {
+  const { cards, accentColor } = props;
+  const cardsCount = cards.length;
+  return (
+    <div>
+      <ul className="cards-list">
+        {cards.map((card, i) => {
+          return (
+            <li key={card.id}>
+              <span className="cards-start-circle"></span>
+              <ItineraryCard card={card} accentColor={accentColor} />
+              {cardsCount !== i + 1 && <Divider />}
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
 }
 
 export default ItineraryCards;
