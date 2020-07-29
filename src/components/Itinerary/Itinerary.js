c8acd7cf33e1c3dbb16282827348ab8a180d201d
Jami Gibbs <jami0821@gmail.com>
2020-07-29 11:13:00 -0500

    Convert Itinerary from class to function.

--- src/components/Itinerary/Itinerary.js
+++ src/components/Itinerary/Itinerary.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BackTop } from 'antd';
 import { isLocalHost } from '../../utils';
 import ReactGA from 'react-ga';
@@ -11,19 +11,20 @@ import { faSuitcase} from '@fortawesome/free-solid-svg-icons'
 import { MehOutlined } from '@ant-design/icons';
 import { ACCENT_COLOR_KEY } from '../../constants';
 
-class Itinerary extends React.Component {
-  state = {
-    isLoading: false,
-    itinerary: {},
-    error: null,
-    accentColor: '#E1474A',
-  }
+const Itinerary = (props) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [itinerary, setItinerary] = useState({});
+  const [error, setError] = useState(null);
+  const [accentColor, setAccentColor] = useState('#E1474A');
 
-  componentDidMount() {
-    const { props } = this.props;
-    const boardShortLink = props.match.params.boardShortLink;
-    this.setState({isLoading: true});
+  useEffect(() => {
+    // const { props } = myProps;
+    const boardShortLink = props.props.match.params.boardShortLink;
+    setIsLoading(true);
+    fetchBoardItinerary(boardShortLink);
+  }, []);
 
+  const fetchBoardItinerary = (boardShortLink) => {
     fetch(`/api/board/${boardShortLink}`)
       .then((response) => response.json())
       .then((itinerary) => {
@@ -32,19 +33,22 @@ class Itinerary extends React.Component {
         const itineraryColorKey = ACCENT_COLOR_KEY + '_' + itinerary.shortLink;
         const accentColor = localStorage.getItem(itineraryColorKey);
         if (accentColor) {
-          this.setState({accentColor});
+          setAccentColor(accentColor);
         }
 
-        this.setState({itinerary, isLoading: false});
+        setItinerary(itinerary);
+        setIsLoading(false);
       })
+      .catch((error) => setError(error));
   }
 
-  handleLoadingIndicator(bool){
-    this.setState({isLoading: bool})
+  const handleLoadingIndicator = (bool) => {
+    setIsLoading(bool);
   }
 
-  updateAccentColor(accentColor){
-    this.setState({accentColor});
+  const updateAccentColor = (accentColor) => {
+    // this.setState({accentColor});
+    setAccentColor(accentColor);
 
     if (!isLocalHost()) {
       ReactGA.event({
@@ -55,65 +59,62 @@ class Itinerary extends React.Component {
     }
 
     // Setting the selected color into local storage.
-    const itineraryColorKey = ACCENT_COLOR_KEY + '_' + this.state.itinerary.shortLink;
+    const itineraryColorKey = ACCENT_COLOR_KEY + '_' + itinerary.shortLink;
     localStorage.setItem(itineraryColorKey, accentColor);
   }
 
-  render() {
-    const { isLoading, itinerary, error, accentColor } = this.state;
-    const isData = itinerary.id;
-    return (
-      <div className="App">
-          <BackTop />
-          
+  const isData = itinerary.id;
+  return (
+    <div className="App">
+        <BackTop />
+        
+        {!error && !isLoading && isData &&
+          <Header headerImages={itinerary.prefs.backgroundImageScaled} 
+                  backgroundColor={itinerary.prefs.backgroundTopColor}
+                  title={itinerary.name} 
+                  lists={itinerary.lists} 
+                  accentColor={accentColor}
+                  handleLoadingIndicator={(bool) => handleLoadingIndicator(bool)}
+                  handleColorUpdated={(hex) => updateAccentColor(hex)}/>
+        }
+
+        <main className="itinerary-content">
+
           {!error && !isLoading && isData &&
-            <Header headerImages={itinerary.prefs.backgroundImageScaled} 
-                    backgroundColor={itinerary.prefs.backgroundTopColor}
-                    title={itinerary.name} 
-                    lists={itinerary.lists} 
-                    accentColor={this.state.accentColor}
-                    handleLoadingIndicator={(bool) => this.handleLoadingIndicator(bool)}
-                    handleColorUpdated={(hex) => this.updateAccentColor(hex)}/>
-          }
-
-          <main className="itinerary-content">
-
-            {!error && !isLoading && isData &&
-              <div>
-                <div className="itinerary-start-circle" style={{backgroundColor: accentColor}}>
-                  <FontAwesomeIcon icon={faSuitcase} color="white" />
-                </div>
-
-                <div className="itinerary-lists">
-                  {itinerary.lists.map(list => {
-                    return (
-                      <div key={list.id} className="itinerary-list">
-                        <div className="itinerary-list-circle" style={{borderColor: accentColor}}></div>
-                          <h2 className="itinerary-list-name" style={{color: accentColor}}>{list.name}</h2>
-                          <ItineraryCards cards={list.cards} accentColor={accentColor} />
-                      </div>
-                    );
-                  })}
-                </div>
+            <div>
+              <div className="itinerary-start-circle" style={{backgroundColor: accentColor}}>
+                <FontAwesomeIcon icon={faSuitcase} color="white" />
               </div>
-            }
 
-          </main>
-
-          {error && 
-            <p className="error-message">{error} &nbsp; <MehOutlined style={{fontSize: '30px'}}/></p> 
-          }
-
-          {!error && isLoading &&
-            <div className="loading-spinner">
-              <LoadingSpinner isLoading={true} />
+              <div className="itinerary-lists">
+                {itinerary.lists.map(list => {
+                  return (
+                    <div key={list.id} className="itinerary-list">
+                      <div className="itinerary-list-circle" style={{borderColor: accentColor}}></div>
+                        <h2 className="itinerary-list-name" style={{color: accentColor}}>{list.name}</h2>
+                        <ItineraryCards cards={list.cards} accentColor={accentColor} />
+                    </div>
+                  );
+                })}
+              </div>
             </div>
           }
 
-      </div>
-    )
-  }
+        </main>
 
+        {error ? 
+          <p className="error-message">{error} &nbsp; <MehOutlined style={{fontSize: '30px'}}/></p> 
+          : null
+        }
+
+        {!error && isLoading &&
+          <div className="loading-spinner">
+            <LoadingSpinner isLoading={true} />
+          </div>
+        }
+
+    </div>
+  )
 }
 
 export default Itinerary;
