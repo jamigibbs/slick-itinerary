b98066e22291b31e2fb81d38a51e660d060aed90
Jami Gibbs <jami0821@gmail.com>
2020-08-06 07:23:56 -0500

    Updating home page layout.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,10 +5,10 @@ import BoardHistory from '../BoardHistory';
 import SiteFooter from '../SiteFooter';
 import './Home.css';
 import { setPageTitle } from '../../utils';
-import { Layout, Row, Col, Divider, Typography } from 'antd';
+import { Layout, Divider, Typography } from 'antd';
 import { BOARD_HISTORY_KEY, TRELLO_URL } from '../../constants';
 
-const { Footer, Content } = Layout;
+const { Footer } = Layout;
 const { Title, Paragraph, Text } = Typography;
 
 const Home = (props) => {
@@ -29,47 +29,42 @@ const Home = (props) => {
   const isBoardHistory = boardHistory ? boardHistory.length > 0 : false;
 
   return (
-    <Layout>
-      <div className="flex-wrapper">
-      <Layout>
-        <Content style={{position: 'relative'}}>
-          <Row className="content-row">
-            <Col xs={{ span: 24, order: 2 }} sm={{ span: 24, order: 2 }} md={{ span: 12, order: 1 }} className="home-left-column-wrap">
-              <div className="home-left-column">
-                <Title className="logo">Slick Itinerary</Title>
-                <Paragraph>
-                Organize your trip using Trello and turn it into a sharable, printable, beautiful travel itinerary instantly!
-                </Paragraph>
+<div>
+      <div className="flex-grid">
 
-                <Divider />
+        <div className="col left">
+          <Title className="logo">Slick Itinerary</Title>
+          <Paragraph>
+          Organize your trip using Trello and turn it into a sharable, printable, beautiful travel itinerary instantly!
+          </Paragraph>
 
-                <Text strong>Quick Start</Text>
+          <Divider />
 
-                <Paragraph>
-                Copy our <a href={`${TRELLO_URL}/b/aMsO3PuO`} aria-label="Example board" target="_blank" rel="noopener noreferrer">example trip board</a> and modify it for your own trip. Then enter your board link below to generate an itineray.
-                </Paragraph>
+          <Text strong>Quick Start</Text>
 
-                <Paragraph style={{marginBottom: '20px'}}>Need more help? Check out the <Link to="guide">support guide</Link>.</Paragraph>
+          <Paragraph>
+          Copy our <a href={`${TRELLO_URL}/b/aMsO3PuO`} aria-label="Example board" target="_blank" rel="noopener noreferrer">example trip board</a> and modify it for your own trip. Then enter your board link below to generate an itineray.
+          </Paragraph>
 
-                <LinkInput />
+          <Paragraph style={{marginBottom: '20px'}}>Need more help? Check out the <Link to="guide">support guide</Link>.</Paragraph>
 
-                <Divider dashed={true} />
+          <LinkInput />
 
-                { isBoardHistory && <BoardHistory data={boardHistory} /> }
-              </div>
-            </Col>
-            <Col className="home-right-column" xs={{ span: 24, order: 1 }} sm={{ span: 24, order: 1 }} md={{ span: 12, order: 2 }} >
-              <img src="img/home-bg.svg" alt="woman riding bike" />
-            </Col>
-          </Row>
-        </Content>
-      </Layout>
-      <Footer style={{backgroundColor: '#333333'}}>
+          <Divider dashed={true} />
+
+          { isBoardHistory && <BoardHistory data={boardHistory} /> }
+        </div>
+
+        <div className="col right">
+          <img src="img/home-bg.svg" alt="woman riding bike" />
+        </div>
+
+      </div>
+
+      <Footer className="footer" style={{backgroundColor: '#333333'}}>
         <SiteFooter></SiteFooter>
       </Footer>
-      </div>
-    </Layout>
-
+    </div>
   )
 }
 
